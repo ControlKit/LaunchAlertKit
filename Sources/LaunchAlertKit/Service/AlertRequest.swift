de34545e83da16b9be96a2d7c0990d493c9e9945
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-09-25 18:39:51 +0330

    Add app version to requests

--- Sources/LaunchAlertKit/Service/AlertRequest.swift
+++ Sources/LaunchAlertKit/Service/AlertRequest.swift
@@ -7,15 +7,16 @@
 
 import Foundation
 public struct AlertRequest {
-    public var appId: String = Bundle.main.bundleIdentifier ?? String()
+    public var appId: String
     public var route: String
+    public var applicationVersion: String = Bundle.main.releaseVersionNumber ?? String()
     public var deviceUUID: String = UUID().uuidString
     public var sdkVersion: String = launchAlertKit_Version
     
     var dictionary: [String: String] {
         return ["x-app-id": appId,
                 "x-sdk-version": sdkVersion,
-                "x-version": "1",
+                "x-version": applicationVersion,
                 "x-device-uuid": deviceUUID]
     }
     
