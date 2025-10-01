bbd113b936a9463efcbacacc20fcf5e42f0e99f5
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-10-02 03:05:27 +0330

    Remove some lines

--- Sources/LaunchAlertKit/Service/AlertResponse.swift
+++ Sources/LaunchAlertKit/Service/AlertResponse.swift
@@ -4,11 +4,8 @@
 //
 //  Created by Maziar Saadatfar on 10/12/23.
 //
-
 import Foundation
-
 public typealias AlertLocalString = [AlertLocalizedText]
-
 public struct AlertResponse: Codable {
     public var data: AlertModel?
 }
